1cf4b7f4d9da3e3ad71197136c9da88508e83301
Destiny <destinylaross@gmail.com>
2021-11-28 11:19:19 -0500

    password reset flow

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,20 +1,23 @@
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
-import { createAuth } from '@keystone-next/auth';
-import { User } from './schemas/User';
-import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { Product } from './schemas/Product';
+import { User } from './schemas/User';
 import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
+
+function check(name: string) {}
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
-  maxAge: 60 * 60 * 24 * 360, // how long signed in
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET,
 };
 
@@ -24,12 +27,19 @@ const { withAuth } = createAuth({
   secretField: 'password',
   initFirstItem: {
     fields: ['name', 'email', 'password'],
-    // TODP add in initial roles
+    // TODO: Add in inital roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      // send the email
+      await sendPasswordResetEmail(args.token, args.identity);
+    },
   },
 });
 
 export default withAuth(
   config({
+    // @ts-ignore
     server: {
       cors: {
         origin: [process.env.FRONTEND_URL],
@@ -40,23 +50,27 @@ export default withAuth(
       adapter: 'mongoose',
       url: databaseURL,
       async onConnect(keystone) {
+        console.log('Connected to the database!');
         if (process.argv.includes('--seed-data')) {
           await insertSeedData(keystone);
         }
       },
     },
     lists: createSchema({
-      // schema items
+      // Schema items go in here
       User,
       Product,
       ProductImage,
     }),
     ui: {
-      // ui only for people who pass test
-      isAccessAllowed: ({ session }) => !!session?.data,
+      // Show the UI only for poeple who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
     },
     session: withItemData(statelessSessions(sessionConfig), {
-      User: 'id',
+      // GraphQL Query
+      User: 'id name email',
     }),
   })
 );
